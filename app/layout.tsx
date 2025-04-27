a9e80d7fd6f86c4cb6b04d8a67b39530be8891b4
Koikbin <kaoming777@gmail.com>
2025-04-27 18:35:31 +0800

    added emblem

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-TW">
+      <head>
+          <title>落石黨 Rockfall Party</title> {/* 這裡是網頁標題 */}
+          {/* <link rel="icon" href="/favicon.ico" /> 這是 favicon 路徑 */}
+          {/* 可選：支援多尺寸 icon */}
+          <link rel="icon" type="image/svg+xml" href="https://averybigwhale.github.io/rockfall/emblem.svg" />
+          {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+      </head>
       <body>
         <NavBar />
         <main>{children}</main>
